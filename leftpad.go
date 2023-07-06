46adf0673b46d4bee5891feafd90167239efe55b
Brian Heidrich <7446405+briandoesdev@users.noreply.github.com>
2023-07-06 14:55:15 -0500

    turns out idk how Go exports module functions

--- leftpad.go
+++ leftpad.go
@@ -2,7 +2,7 @@ package leftpad
 
 import "bytes"
 
-func leftpad(str string, length int, ch byte) string {
+func Leftpad(str string, length int, ch byte) string {
 	if len(str) >= length {
 		return str
 	}
